380561a50f51e304e6ccba5e07dc2414f9028a4c
Gonzalo <g.caniupan01@ufromail.cl>
2021-09-04 19:01:15 -0400

    Proyecto de Prueba

--- src/com/example/primerProyecto.java
+++ src/com/example/primerProyecto.java
@@ -3,6 +3,7 @@ package com.example;
 public class primerProyecto {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("Gonzalo Felipe Caniupan Huenuche");
 
     }
 
